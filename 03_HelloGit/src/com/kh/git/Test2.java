b5414c947fadcf95c649f4acf525f8efc0fe0d65
jjsk109 <jjsk109@gmail.com>
2018-06-12 13:22:17 +0900

    이것저것

--- 03_HelloGit/src/com/kh/git/Test2.java
+++ 03_HelloGit/src/com/kh/git/Test2.java
@@ -4,6 +4,14 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("아싸!!!!");
+		int e = 8;
+		for(int i = 0 ; i<e; i++) {
+			System.out.print(i+" : ");
+			if((i*e)/2!=0) {
+				System.out.println(i);
+			}
+		}
+		
 	}
 
 }
